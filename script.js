ece2b84dcb4924e08dcf760d52a3275662bb1641
Nathan Musial <nathan.ank@hotmail.fr>
2024-03-21 11:06:05 +0100

    transformation de l'animation carte en fenêtre popup

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ fetch("https://rickandmortyapi.com/api/character")
 .then(response => response.json())
 .then(data => generateCards(data.results))
 
+function getEpisodeList(episodeURLs) {
+    return episodeURLs.map(url => {
+        const episodeNumber = url.split('/').pop(); // Obtient le numéro de l'épisode à partir de l'URL
+        return `${episodeNumber}`;
+    }).join(', ');
+};
+
+function togglePopup(characterId) {
+    const popup = document.querySelector(`.popup-card.character-card-${characterId}`);
+    popup.classList.toggle("open");
+    
+    // Récupérer tous les popups
+    const allPopups = document.querySelectorAll('.popup-card');
+}
+
 function generateCards(charactersArray) {
     const cardContainer = document.querySelector(".cardContainer");
     charactersArray.forEach(character => {
         const card = document.createElement('div');
-        card.className = 'flip-card m-5';
+        card.className = 'flip-card';
 
         card.innerHTML = `
-            <div class="flip-card-inner">
 
                 <div class="character-card-${character.id} flip-card-front">
                     <div class="flex justify-center">
-                        <img src=${character.image}></img>
+                        <img class="w-64" src=${character.image}></img>
                     </div>
                     <div class="text-white mx-8 my-5 leading-7">
                         <h2>Name : ${character.name}</h2>
@@ -23,54 +37,35 @@ function generateCards(charactersArray) {
                     </div>
                     <div class="flex justify-center items-baseline">
                         <button class="card-button">Know more</button>
-                    </div>
-                </div>
-    
-                <div class="character-card-${character.id} flip-card-back">
-                    <div class="flex justify-center">
-                        <img class="w-40 h-auto" src=${character.image}></img>
-                    </div>
-                    <div class="text-white mx-8 my-5 leading-7">
-                        <h2>Name : ${character.name}</h2>
-                        <p>Origin : ${character.origin.name}</p>
-                        <p>Last known location : ${character.location.name}</p>
-                        <p>Species : ${character.species}</p>
-                        <p>Episodes : ${getEpisodeList(character.episode)}</p>
-                    </div>
-                    <div class="flex justify-center items-start pb-1">
-                        <button class="back-button">Flip back</button>
-                    </div>
                 </div>
 
+            <div id="popup-${character.id}" class="popup-card character-card-${character.id}"> <!-- Ajout de la classe popup-card ici -->
+                <div class="flex justify-center">
+                    <img class="w-64 h-auto" src=${character.image}></img>
+                </div>
+                <div class="text-white mx-8 my-5 leading-7">
+                    <h2>Name : ${character.name}</h2>
+                    <p>Origin : ${character.origin.name}</p>
+                    <p>Last known location : ${character.location.name}</p>
+                    <p>Species : ${character.species}</p>
+                    <p>Episodes : ${getEpisodeList(character.episode)}</p>
+                </div>
+                <div class="flex justify-center items-start pb-1">
+                    <button class="popup-button">Close</button>
+                </div>
             </div>`;
-        
+
         cardContainer.appendChild(card);
+        
+        // Ajout des écouteurs d'événements pour les boutons .card-button et .back-button
+        const cardButton = card.querySelector('.card-button');
+        cardButton.addEventListener('click', function() {
+            togglePopup(character.id);
+        });
+
+        const backButton = card.querySelector('.popup-button');
+        backButton.addEventListener('click', function() {
+            togglePopup(character.id);
+        });
     });
-
-    // Ajouter l'écouteur d'événements pour le flip après avoir créé toutes les cartes
-    const cardButtons = document.querySelectorAll('.card-button');
-    cardButtons.forEach(button => {
-        button.addEventListener('click', toggleCard);
-    });
-
-}
-
-function getEpisodeList(episodeURLs) {
-    return episodeURLs.map(url => {
-        const episodeNumber = url.split('/').pop(); // Obtient le numéro de l'épisode à partir de l'URL
-        return `${episodeNumber}`;
-    }).join(', ');
-}
-
-function toggleCard() {
-    console.log("j'ai cliqué");
-
-    // Récupère l'élément .flip-card-inner
-    const flipCardInner = this.closest('.flip-card').querySelector('.flip-card-inner');
-    
-    // Ajoute ou retire la classe .flip pour effectuer l'animation
-    flipCardInner.classList.toggle('flip');
-
-    // Ajoute ou retire la classe .flip-card-back pour afficher la face arrière ou avant de la carte
-    flipCardInner.classList.toggle('flip-card-back');
 }
